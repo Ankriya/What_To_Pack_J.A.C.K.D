cdfc23be4c32d43f085d4e363cbb05553b328205
Kyle Jeremiah Govender <129373267+ST10145498@users.noreply.github.com>
2025-10-01 19:24:07 +0200

    Updated My Suitcase and Trip Details activity Navigation

--- app/src/main/java/student/projects/prog7312_poe_jackd/MySuitcaseActivity.kt
+++ app/src/main/java/student/projects/prog7312_poe_jackd/MySuitcaseActivity.kt
@@ -1,7 +1,8 @@
 package student.projects.prog7312_poe_jackd
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -9,8 +10,18 @@ import androidx.core.view.WindowInsetsCompat
 class MySuitcaseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_my_suitcase)
+
+        // Trip Details button
+        findViewById<Button>(R.id.TripDetailsBtn).setOnClickListener {
+            startActivity(Intent(this, TripDetailsActivity::class.java))
+        }
+
+        // My List button
+        findViewById<Button>(R.id.MyListBtn).setOnClickListener {
+            startActivity(Intent(this, MyListActivity::class.java))
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/student/projects/prog7312_poe_jackd/TripDetailsActivity.kt
+++ app/src/main/java/student/projects/prog7312_poe_jackd/TripDetailsActivity.kt
@@ -1,7 +1,8 @@
 package student.projects.prog7312_poe_jackd
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -9,8 +10,13 @@ import androidx.core.view.WindowInsetsCompat
 class TripDetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_trip_details)
+
+        // View Trips button navigation
+        findViewById<Button>(R.id.ViewTripsBtn).setOnClickListener {
+            startActivity(Intent(this, ViewTripsActivity::class.java))
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
