f2beab6d4cdf158711698a4268676b220d9bd215
Kyle Jeremiah Govender <129373267+ST10145498@users.noreply.github.com>
2025-10-01 19:03:08 +0200

    Updated My Search Activity -Added the navigation to the "My Search" Activity Page

--- app/src/main/java/student/projects/prog7312_poe_jackd/MySearchActivity.kt
+++ app/src/main/java/student/projects/prog7312_poe_jackd/MySearchActivity.kt
@@ -1,7 +1,9 @@
 package student.projects.prog7312_poe_jackd
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -9,8 +11,33 @@ import androidx.core.view.WindowInsetsCompat
 class MySearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_my_search)
+
+        // Events button
+        findViewById<Button>(R.id.EventsBtn).setOnClickListener {
+            startActivity(Intent(this, EventsActivity::class.java))
+        }
+
+        // Weather button
+        findViewById<Button>(R.id.WeatherBtn).setOnClickListener {
+            startActivity(Intent(this, WeatherActivity::class.java))
+        }
+
+        // Restaurant button
+        findViewById<Button>(R.id.RestaurantBtn).setOnClickListener {
+            startActivity(Intent(this, RestaurantActivity::class.java))
+        }
+
+        // Transport TextView (clickable)
+        findViewById<TextView>(R.id.TransportBtn).setOnClickListener {
+            startActivity(Intent(this, TransportActivity::class.java))
+        }
+
+        // Currency TextView (clickable)
+        findViewById<TextView>(R.id.CurrencyBtn).setOnClickListener {
+            startActivity(Intent(this, CurrencyActivity::class.java))
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
--- app/src/main/java/student/projects/prog7312_poe_jackd/TransportActivity.kt
+++ app/src/main/java/student/projects/prog7312_poe_jackd/TransportActivity.kt
@@ -10,7 +10,7 @@ class TransportActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_weather)
+        setContentView(R.layout.activity_transport)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
