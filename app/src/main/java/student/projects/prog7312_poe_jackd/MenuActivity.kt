0ce967dae5f163b8cb91b7be81a6fd6231264bfc
Kyle Jeremiah Govender <129373267+ST10145498@users.noreply.github.com>
2025-10-01 17:12:49 +0200

    Updated Menu Activity -Added the navigation to the Menu Activity Page

--- app/src/main/java/student/projects/prog7312_poe_jackd/MenuActivity.kt
+++ app/src/main/java/student/projects/prog7312_poe_jackd/MenuActivity.kt
@@ -1,7 +1,8 @@
 package student.projects.prog7312_poe_jackd
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -9,8 +10,18 @@ import androidx.core.view.WindowInsetsCompat
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_menu)
+
+        // SEARCH button navigation
+        findViewById<Button>(R.id.SearchBtn).setOnClickListener {
+            startActivity(Intent(this, MySearchActivity::class.java))
+        }
+
+        // MY SUITCASE button navigation
+        findViewById<Button>(R.id.SuitcaseBtn).setOnClickListener {
+            startActivity(Intent(this, MySuitcaseActivity::class.java))
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
